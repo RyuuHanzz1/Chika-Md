f32e007dc37bde83ba0aa0e0b64ba1058f5332ac
RyuuHanzz1 <105896846+RyuuHanzz1@users.noreply.github.com>
2022-06-18 13:47:19 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,14 +19,14 @@
   }
 
   // Setting Owner
-  global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.owner = ['628889312587','628889312587','6285773906324'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.premium = ['628889312587'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.ownernomer = '628889312587' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
   // Set Nama Own & Bot
-  global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-  global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.ownername = 'han' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.botname = 'HanBot' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.footer = 'Hanz' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Kebutuhan Button & Kontak
   //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
@@ -36,8 +36,8 @@
   global.region = 'indonesia'
 
   // Set Wm
-  global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.packname = '©Pler' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.author = 'Hanz' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Nama Session
   //gausah di apa² in!
